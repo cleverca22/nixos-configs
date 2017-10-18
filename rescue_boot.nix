74ea27b3a85212c21acaadccd03e907554028e08
michael bishop <cleverca22@gmail.com>
2017-10-18 15:09:33 -0300

    add a rescue boot menu option

---
+++ rescue_boot.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+let
+  netboot = import (pkgs.path + "/nixos/lib/eval-config.nix") {
+      # you will want to add options here to support your filesystem
+      # and also maybe ssh to let you in
+      modules = [
+        (pkgs.path + "/nixos/modules/installer/netboot/netboot-minimal.nix")
+      ];
+    };
+in {
+  boot.loader.grub.extraEntries = ''
+    menuentry "Nixos Installer" {
+      linux ($drive1)/rescue-kernel init=${netboot.config.system.build.toplevel}/init ${toString netboot.config.boot.kernelParams}
+      initrd ($drive1)/rescue-initrd
+    }
+  '';
+  boot.loader.grub.extraFiles = {
+    "rescue-kernel" = "${netboot.config.system.build.kernel}/bzImage";
+    "rescue-initrd" = "${netboot.config.system.build.netbootRamdisk}/initrd";
+  };
+}
