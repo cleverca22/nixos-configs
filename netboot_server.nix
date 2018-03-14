f5ff3c09b34a943ff0c610d020f0668c6a0ecd95
michael bishop <cleverca22@gmail.com>
2018-03-14 09:53:01 -0300

    partial uefi support

--- netboot_server.nix
+++ netboot_server.nix
@@ -10,15 +10,34 @@ let
       modules = [
         (pkgs.path + "/nixos/modules/installer/netboot/netboot-minimal.nix")
         ./nix-tests/kexec/justdoit.nix
+        module
       ];
     }).config.system.build;
   in pkgs.symlinkJoin {
     name = "netboot";
     paths = with build; [ netbootRamdisk kernel netbootIpxeScript ];
   };
+  module = {
+    kexec.justdoit = {
+      luksEncrypt = true;
+      rootDevice = "/dev/vda";
+      swapSize = 40 * 1024;
+      bootSize = 512;
+    };
+  };
+  ipxe' = pkgs.ipxe.overrideDerivation (drv: {
+    installPhase = ''
+      ${drv.installPhase}
+      make $makeFlags bin-x86_64-efi/ipxe.efi bin-i386-efi/ipxe.efi
+      cp -v bin-x86_64-efi/ipxe.efi $out/x86_64-ipxe.efi
+      cp -v bin-i386-efi/ipxe.efi $out/i386-ipxe.efi
+    '';
+  });
   tftp_root = pkgs.runCommand "tftproot" {} ''
     mkdir -pv $out
-    cp -vi ${pkgs.ipxe}/undionly.kpxe $out/undionly.kpxe
+    cp -vi ${ipxe'}/undionly.kpxe $out/undionly.kpxe
+    cp -vi ${ipxe'}/x86_64-ipxe.efi $out/x86_64-ipxe.efi
+    cp -vi ${ipxe'}/i386-ipxe.efi $out/i386-ipxe.efi
   '';
   nginx_root = pkgs.runCommand "nginxroot" {} ''
     mkdir -pv $out
@@ -58,6 +77,7 @@ in {
         interfaces = [ cfg.network.lan ];
         enable = true;
         extraConfig = ''
+          option arch code 93 = unsigned integer 16;
           subnet 192.168.3.0 netmask 255.255.255.0 {
             option domain-search "localnetboot";
             option subnet-mask 255.255.255.0;
@@ -69,7 +89,11 @@ in {
             if exists user-class and option user-class = "iPXE" {
               filename "http://192.168.3.1/boot.php?mac=''${net0/mac}&asset=''${asset:uristring}&version=''${builtin/version}";
             } else {
-              filename = "undionly.kpxe";
+              if option arch = 00:07 or option arch = 00:09 {
+                filename = "x86_64-ipxe.efi";
+              } else {
+                filename = "undionly.kpxe";
+              }
             }
           }
         '';
