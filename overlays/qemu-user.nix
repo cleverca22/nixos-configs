bfeaeff70d0f114801d2aff2f0d60dbca321578f
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-04-21 23:56:20 +0800

    Moved the conditional check into the overlays

--- overlays/qemu-user.nix
+++ overlays/qemu-user.nix
@@ -1,7 +1,9 @@
 self: super:
 
 {
-  qemu-user-arm = self.callPackage ./qemu { user_arch = "arm"; };
+  qemu-user-arm = if self.stdenv.system == "x86_64-linux"
+    then self.callPackage_i686 ./qemu { user_arch = "arm"; }
+    else self.callPackage      ./qemu { user_arch = "arm"; };
   qemu-user-x86 = self.callPackage ./qemu { user_arch = "x86_64"; };
   qemu-user-arm64 = self.callPackage ./qemu { user_arch = "aarch64"; };
   qemu-user-riscv32 = self.callPackage ./qemu { user_arch = "riscv32"; };
