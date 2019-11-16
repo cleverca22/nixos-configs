96fcb2a6ed7b8ab43d1f0c157cdb53714d29fe08
Michael Bishop <cleverca22@gmail.com>
2019-11-16 00:31:22 -0400

    add plex frontend to release

--- release.nix
+++ release.nix
@@ -5,4 +5,7 @@ in {
   nas = (nixosFunc { configuration = ./nas.nix; }).system;
   router = (nixosFunc { configuration = ./router.nix; }).system;
   nix-tar.arm = linuxPkgs.callPackage ./arm-tar.nix {};
+  software = { # things used by several machines
+    inherit (linuxPkgs) plex-media-player;
+  };
 }
