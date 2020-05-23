e0aa5260c63ee7985c1e25a022f2bac72450c892
Michael Bishop <cleverca22@gmail.com>
2020-05-23 15:51:56 -0300

    switch the module over to niv

--- arcstats.nix
+++ arcstats.nix
@@ -2,12 +2,8 @@
 
 with lib;
 let
-  arcstats = pkgs.callPackage (pkgs.fetchFromGitHub {
-    owner = "cleverca22";
-    repo = "arcstats";
-    rev = "0f84957053bbbd62b753189b779767c5fd863e98";
-    sha256 = "1126685cjskf65jwd2jddr7wiknr9nw9afm4ajrf6ss292yicwsb";
-  }) {};
+  sources = import ./nix/sources.nix;
+  arcstats = pkgs.callPackage sources.arcstats {};
 in {
   options = {
     services.arcstats = mkEnableOption "arcstats";
