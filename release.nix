bf514b56643cd01fca3feb03dfa2b70597ce69c1
Michael Bishop <cleverca22@gmail.com>
2020-05-23 14:44:51 -0300

    build arcstats seperately

--- release.nix
+++ release.nix
@@ -1,11 +1,13 @@
 let
   nixosFunc = import <nixpkgs/nixos>;
   linuxPkgs = import <nixpkgs> {};
+  sources = import ./nix/sources.nix;
 in {
   nas = (nixosFunc { configuration = ./nas.nix; }).system;
   router = (nixosFunc { configuration = ./router.nix; }).system;
   nix-tar.arm = linuxPkgs.callPackage ./arm-tar.nix {};
   software = { # things used by several machines
     inherit (linuxPkgs) plex-media-player rtorrent;
+    arcstats = linuxPkgs.callPackage sources.arcstats {};
   };
 }
