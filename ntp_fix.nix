7322b81f4fa7271c015014ed44595030f700b862
Michael Bishop <cleverca22@gmail.com>
2020-12-14 20:43:13 -0400

    just disable seccomp

--- ntp_fix.nix
+++ ntp_fix.nix
@@ -2,8 +2,11 @@
 
 let
   overlay = self: super: {
-    ntp = super.ntp.overrideAttrs (drv: {
-      patches = [ (self.fetchpatch { url = "https://github.com/ntp-project/ntp/commit/881e427f3236046466bdb8235edf86e6dfa34391.patch"; sha256 = "0iqn12m7vzsblqbds5jb57m8cjs30rw8nh2xv8k2g8lbqbyk1k7s"; }) ];
+    #ntp = super.ntp.overrideAttrs (drv: {
+    #  patches = [ (self.fetchpatch { url = "https://github.com/ntp-project/ntp/commit/881e427f3236046466bdb8235edf86e6dfa34391.patch"; sha256 = "0iqn12m7vzsblqbds5jb57m8cjs30rw8nh2xv8k2g8lbqbyk1k7s"; }) ];
+    #});
+    ntp = super.ntp.overrideAttrs (old: {
+      configureFlags = old.configureFlags ++ [ "--disable-libseccomp" ];
     });
   };
 in {
