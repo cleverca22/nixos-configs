845c6f694f463a609151d2367396ae30373365b1
Michael Bishop <cleverca22@gmail.com>
2022-07-11 18:54:34 -0300

    add direnv.nix

---
+++ direnv.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    #nix-direnv
+    direnv
+  ];
+  nix.extraOptions = ''
+    # direnv.nix
+    #keep-outputs = true
+    keep-derivations = true
+  '';
+  environment.pathsToLink = [
+    #"/share/nix-direnv"
+  ];
+  programs.bash.interactiveShellInit = ''
+    if [ -z $IN_NIX_SHELL ]; then
+      eval "$(direnv hook bash)"
+    fi
+  '';
+}
