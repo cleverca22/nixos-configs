b6c9f1e8cfae5e4201b7d3b96aa67fe9a991d4b9
Michael Bishop <michael.bishop@iohk.io>
2023-11-12 00:53:39 -0400

    also install steam-run and steamcmd

--- steam.nix
+++ steam.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.steam ];
+  environment.systemPackages = [ pkgs.steam pkgs.steamcmd pkgs.steam-run ];
   hardware.pulseaudio.support32Bit = true;
   hardware.opengl.driSupport32Bit = true;
 }
