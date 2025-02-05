d789e5952943d321df6fd7d6a665330024be7564
Michael Bishop <cleverca22@gmail.com>
2025-02-04 21:46:52 -0400

    test

--- test.sh
+++ test.sh
@@ -1,11 +1,7 @@
-pwd
-ls -l
-env
-ps aux
-
-
-curl https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_WORKFLOW_SHA} \
-  -v -L -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -d '{"state":"success","context":"CLI"}'
+for n in $(seq 1 20); do
+  curl https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_WORKFLOW_SHA} \
+    -L -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -d '{"state":"success","context":"CLI-${n}","target_url":"https://example.com/${n}","description":"test ${n}"}'
+done
