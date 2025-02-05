9d31dce0e7e962c5c03f6781ae48b326a2f46071
Michael Bishop <cleverca22@gmail.com>
2025-02-04 21:49:13 -0400

    test

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ for n in $(seq 1 20); do
     -s -L -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    -d '{"state":"success","context":"CLI-${n}","target_url":"https://example.com/${n}","description":"test ${n}"}'
+    -d '{"state":"success","context":"CLI-'${n}'","target_url":"https://example.com/'${n}'","description":"test '${n}'"}'
 done
