3c4c8af0ca4ac7004eff58cbf6c4e5b32df66a1e
Michael Bishop <cleverca22@gmail.com>
2025-02-04 21:47:39 -0400

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 for n in $(seq 1 20); do
   curl https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_WORKFLOW_SHA} \
-    -L -X POST \
+    -s -L -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
     -d '{"state":"success","context":"CLI-${n}","target_url":"https://example.com/${n}","description":"test ${n}"}'
