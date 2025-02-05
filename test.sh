1437db7c9e75cbd1f29fcc8e6e83b38e0d4bfc80
Michael Bishop <cleverca22@gmail.com>
2025-02-04 21:10:42 -0400

    test

--- test.sh
+++ test.sh
@@ -2,3 +2,10 @@ pwd
 ls -l
 env
 ps aux
+
+
+curl https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_WORKFLOW_SHA} \
+  -v -L -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -d '{"state":"success","context":"CLI"}'
