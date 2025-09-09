a9e5d9932f409e0e2f72605eb607bcf50f5db17d
Michael Bishop <cleverca22@gmail.com>
2025-09-09 13:07:53 -0300

    update zfs patch

--- zfs-patch.nix
+++ zfs-patch.nix
@@ -3,11 +3,11 @@
 let
   zfs-src = pkgs.fetchurl {
     url = "https://github.com/openzfs/zfs/archive/pull/14013/head.tar.gz";
-    hash = "sha256-C+1fxxLSKLh0pOY/f7BguYlfbsyxiyKuk2lAkJMkFfE=";
+    hash = "sha256-pQBkzpOe/Os7Rca5CbPmFmGLqY0Zb9v29tp7EA2Bla4=";
   };
 in {
-  boot.kernelPackages = pkgs.linuxPackages_5_15.extend (self: super: {
-    zfs_2_2 = super.zfs_2_2.overrideAttrs (old: {
+  boot.kernelPackages = pkgs.linuxPackages.extend (self: super: {
+    zfs_2_3 = super.zfs_2_3.overrideAttrs (old: {
       src = zfs-src;
     });
   });
