27a81c0b609265a70d4d3618a4cb7527a1c71835
Michael Bishop <cleverca22@gmail.com>
2025-11-07 16:46:20 -0400

    fix nix arm tar

--- arm-tar.nix
+++ arm-tar.nix
@@ -3,7 +3,7 @@ callPackage <nixpkgs/nixos/lib/make-system-tarball.nix> {
   fileName = "nix_tarball";
   storeContents = [
     {
-      object = (import <nixpkgs> {}).pkgsCross.armv7l-hf-multiplatform.nix.override { withAWS = false; };
+      object = (import <nixpkgs> {}).pkgsCross.armv7l-hf-multiplatform.nix.override { };
       symlink = "/nix-built";
     }
   ];
